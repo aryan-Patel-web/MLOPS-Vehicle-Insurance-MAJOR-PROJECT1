e9bcce22220da190f13ef412aa95b6689ebb841a
aryan-Patel-web <aryanpatel77462@gmail.com>
2026-02-27 02:13:31 +0530

    logging added in file __init__.py  folder path=src/logger/file.py done ==

--- src/logger/__init__.py
+++ src/logger/__init__.py
@@ -12,6 +12,7 @@ MAX_LOG_SIZE = 5 * 1024 * 1024  # 5 MB
 BACKUP_COUNT = 3  # Number of backup log files to keep
 
 # Construct log file path
+
 log_dir_path = os.path.join(from_root(), LOG_DIR)
 os.makedirs(log_dir_path, exist_ok=True)
 log_file_path = os.path.join(log_dir_path, LOG_FILE)
