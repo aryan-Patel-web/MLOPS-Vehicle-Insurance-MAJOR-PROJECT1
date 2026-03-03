a539648d98bcebed2832fbfc6d645c2f014f7075
aryan-Patel-web <aryanpatel77462@gmail.com>
2026-03-03 09:39:35 +0530

    make AWS connection and create IAM user  done and S3 bucket creation also done = now its time for adding connection code of AWS ==

--- src/configuration/aws_connection.py
+++ src/configuration/aws_connection.py
@@ -14,8 +14,10 @@ class S3Client:
         """
 
         if S3Client.s3_resource==None or S3Client.s3_client==None:
+
             __access_key_id = os.getenv(AWS_ACCESS_KEY_ID_ENV_KEY, )
             __secret_access_key = os.getenv(AWS_SECRET_ACCESS_KEY_ENV_KEY, )
+            
             if __access_key_id is None:
                 raise Exception(f"Environment variable: {AWS_ACCESS_KEY_ID_ENV_KEY} is not not set.")
             if __secret_access_key is None:
@@ -26,10 +28,12 @@ class S3Client:
                                             aws_secret_access_key=__secret_access_key,
                                             region_name=region_name
                                             )
+            
             S3Client.s3_client = boto3.client('s3',
                                         aws_access_key_id=__access_key_id,
                                         aws_secret_access_key=__secret_access_key,
                                         region_name=region_name
                                         )
+            
         self.s3_resource = S3Client.s3_resource
         self.s3_client = S3Client.s3_client
