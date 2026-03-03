fc02f42b44d0609da6030e32a12485c554523950
aryan-Patel-web <aryanpatel77462@gmail.com>
2026-03-03 10:20:03 +0530

    now addd model evaluation file

--- src/components/model_evaluation.py
+++ src/components/model_evaluation.py
@@ -1,13 +1,17 @@
 from src.entity.config_entity import ModelEvaluationConfig
 from src.entity.artifact_entity import ModelTrainerArtifact, DataIngestionArtifact, ModelEvaluationArtifact
+
 from sklearn.metrics import f1_score
 from src.exception import MyException
-from src.constants import TARGET_COLUMN
+from src.constants import TARGET_COLUMN    ## target column name= response
+
 from src.logger import logging
 from src.utils.main_utils import load_object
+
 import sys
 import pandas as pd
 from typing import Optional
+
 from src.entity.s3_estimator import Proj1Estimator
 from dataclasses import dataclass
 
