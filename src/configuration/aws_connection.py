6107f45a718356f1361dedc5aa58368d7b36d8e4
aryan-Patel-web <aryanpatel77462@gmail.com>
2026-03-08 17:46:04 +0530

    aws key and secret kay changes ----

--- src/configuration/aws_connection.py
+++ src/configuration/aws_connection.py
@@ -1,11 +1,11 @@
 import boto3
 import os
 from src.constants import AWS_SECRET_ACCESS_KEY_ENV_KEY, AWS_ACCESS_KEY_ID_ENV_KEY, REGION_NAME
-
 class S3Client:
 
     s3_client=None
     s3_resource = None
+    
     def __init__(self, region_name=REGION_NAME):
         """ 
         This Class gets aws credentials from env_variable and creates an connection with s3 bucket 
@@ -33,6 +33,7 @@ class S3Client:
                                         aws_secret_access_key=__secret_access_key,
                                         region_name=region_name
                                         )
+            
             
         self.s3_resource = S3Client.s3_resource
         self.s3_client = S3Client.s3_client
